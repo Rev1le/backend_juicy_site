104f52f54b069f0f980379192a277c13ad56d589
Rev1le <rev1le@vivaldi.net>
2022-10-07 21:52:38 +0500

    Add *get_val_new*

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,13 @@ mod sqlite_conn;
 
 use rocket::{
     http::ContentType,
-    serde::{Serialize, json::Json},
+    serde::{Serialize, Deserialize, json::Json},
     fs::{NamedFile}
 };
-use std::{
-    fs,
-    collections::HashMap
-};
-use crate::sqlite_conn::user::{User};
+use std::{fs, collections::HashMap, result};
+use rocket::data::N;
+use rocket::serde::json::serde_json;
+use crate::sqlite_conn::user;
 use crate::sqlite_conn::document::Document;
 use crate::sqlite_conn::DataBase;
 
@@ -27,6 +26,11 @@ fn index() -> (ContentType, String) {
     (ContentType::HTML, html)
 }
 
+#[get("/")] // Для отображения списка api адресов
+async fn all_api() -> Json<Vec<&'static str>> {
+    Json(vec!["/get_photo", "/upload_files?<url>", "/get"])
+}
+
 #[get("/get_photo?<name>")]
 pub async fn get_files(name: Option<&str>) -> Option<NamedFile> {
     // Добавить првоерку того, что запрашивавется фото
@@ -37,7 +41,7 @@ pub async fn get_files(name: Option<&str>) -> Option<NamedFile> {
 	None
 }
 
-#[derive(Debug, PartialEq, FromForm)]
+#[derive(Debug, PartialEq, FromForm, Serialize)]
 struct UserFromRequest<'a> {
     name: Option<&'a str>,
     nickname: Option<&'a str>,
@@ -58,28 +62,70 @@ struct DocumentFromRequest<'a> {
     number_work: Option<&'a str>,
 }
 
-#[get("/")] // Для отображения списка api адресов
-async fn all_api() -> Json<Vec<&'static str>> {
-    Json(vec!["/get_photo", "/upload_files?<url>", "/get"])
-}
-
-#[get("/get_all_users")]
-fn get_all_users() -> Json<Option<Vec<User>>> {
-    let db = DataBase::new(PATH_BD);
-    Json(Some(db.get_all_user()))
-}
+//#[derive(Debug, Serialize, Clone)]
+//enum Respone {
+//    Users(Vec<user::User>),
+//    Docs(Vec<Document>),
+//    Nill
+//}
 
 #[derive(Debug, Serialize, Clone)]
-enum ResponeDocUser {
-    ResponeUser(User),
-    ResponeDoc(Document)
+struct Respone {
+    Users: Vec<user::User>,
+    Docs: Vec<Document>
 }
 
+#[get("/get?<user>&<doc>&<all_users>&<all_docs>")]
+async fn get_val_new(
+    user: Option<UserFromRequest<'_>>,
+    doc: Option<DocumentFromRequest<'_>>,
+    all_users: Option<bool>,
+    all_docs: Option<bool>
+) -> Json<Respone> {
 
-#[get("/get?<user>&<doc>")]
+    let mut respone = Respone{Users: Vec::with_capacity(4), Docs: Vec::with_capacity(4)};
+
+    // Работа с поиском пользователей
+    if let Some(true) = all_users {
+        let db = DataBase::new(PATH_BD);
+
+        if let Some(user_vec) = db.get_user(HashMap::new()) {
+            respone.Users.extend_from_slice(&user_vec);
+        }
+
+    } else {
+        let mut result_user_vec: Vec<user::User> = Vec::with_capacity(4);
+        if let Some(user) = user {
+            respone.Users.extend_from_slice(&check_user(user));
+        }
+    }
+
+    // Работа с поиском документов
+    if let Some(true) = all_docs {
+        let db = DataBase::new(PATH_BD);
+
+        if let Some(docs_vec) = db.get_doc(&HashMap::<&str, &str>::new()) {
+            respone.Docs.extend_from_slice(&docs_vec);
+        }
+
+    } else {
+        let mut result_doc_vec: Vec<Document> = Vec::with_capacity(4);
+        if let Some(doc) = doc {
+            respone.Docs.extend_from_slice(&check_doc(doc));
+        }
+    }
+
+    return Json(respone);
+}
+
+/*
+#[get("/get?<user>&<doc>&<all_users>&<all_docs>")]
 async fn get_val(
     user: Option<UserFromRequest<'_>>,
-    doc: Option<DocumentFromRequest<'_>>) -> Json<Vec<ResponeDocUser>> {
+    doc: Option<DocumentFromRequest<'_>>,
+    all_users: Option<bool>,
+    all_docs: Option<bool>
+) -> Json<Vec<ResponeDocUser>> {
 
     let mut result_vec: Vec<ResponeDocUser> = Vec::with_capacity(4);
 
@@ -89,120 +135,162 @@ async fn get_val(
     if let Some(doc) = doc {
         result_vec.extend_from_slice(&check_doc(doc));
     }
-    Json(result_vec)
-}
 
-fn check_user(user: UserFromRequest) -> Vec<ResponeDocUser>{
-    println!(" Пользователь {:?}", user);
-
-    let mut vec_result_user: Vec<ResponeDocUser> = Vec::new();
-    let mut clear_user: bool = true;
-    let mut hm = HashMap::new();
-
-    if let Some(name) = user.name {
-        hm.insert("name", name);
-        clear_user = false;
-    }
-    if let Some(nickname) = user.nickname {
-        hm.insert("nickname", nickname);
-        clear_user = false;
-    }
-    if let Some(avatar) = user.avatar {
-        hm.insert("avatar", avatar);
-        clear_user = false;
-    }
-    if let Some(role) = user.role {
-        hm.insert("role", role);
-        clear_user = false;
-    }
-    if let Some(admin) = user.admin {
-        hm.insert("admin", admin);
-        clear_user = false;
-    }
-    if let Some(tg_id) = user.tg_id {
-        hm.insert("tg_id", tg_id);
-        clear_user = false;
-    }
-    if let Some(uuid) = user.uuid {
-        hm.insert("uuid", uuid);
-        clear_user = false;
-    }
-    if !clear_user {
+    if let Some(true) = all_users {
+        let mut vec_result_user: Vec<ResponeDocUser> = Vec::new();
         let db = DataBase::new(PATH_BD);
-        if let Some(user_vec) = db.get_user(hm) {
+
+        if let Some(user_vec) = db.get_user(HashMap::new()) {
             for user in user_vec {
                 let tmp = user.clone();
                 vec_result_user.push(ResponeDocUser::ResponeUser(tmp))
             }
+            return Json(vec_result_user)
         }
     }
-    vec_result_user
+
+    if let Some(true) = all_docs {
+        let mut vec_result_doc: Vec<ResponeDocUser> = Vec::new();
+        let mut hm = HashMap::new();
+        let db = DataBase::new(PATH_BD);
+
+        if let Some(doc_vec) = db.get_doc(hm) {
+            for doc in doc_vec {
+                let tmp = doc.clone();
+                vec_result_doc.push(ResponeDocUser::ResponeDoc(tmp))
+            }
+            return Json(vec_result_doc)
+        }
+    }
+
+    Json(result_vec)
 }
 
-fn check_doc(doc: DocumentFromRequest) -> Vec<ResponeDocUser>{
+ */
+
+fn check_user(user: UserFromRequest) -> Vec<user::User>{
+    println!(" Пользователь {:?}", user);
+    let mut hm = HashMap::new();
+
+    if let Some(name) = user.name {
+        hm.insert("name", name);
+    }
+    if let Some(nickname) = user.nickname {
+        hm.insert("nickname", nickname);
+    }
+    if let Some(avatar) = user.avatar {
+        hm.insert("avatar", avatar);
+    }
+    if let Some(role) = user.role {
+        hm.insert("role", role);
+    }
+    if let Some(admin) = user.admin {
+        hm.insert("admin", admin);
+    }
+    if let Some(tg_id) = user.tg_id {
+        hm.insert("tg_id", tg_id);
+    }
+    if let Some(uuid) = user.uuid {
+        hm.insert("uuid", uuid);
+    }
+
+    if hm.len() != 0 {  // Если никакие данные не были вставлены,
+                        // значит пользователь не запрашивался
+        let db = DataBase::new(PATH_BD);
+        if let Some(user_vec) = db.get_user(hm) {
+            return user_vec
+        }
+    }
+    return Vec::new()
+}
+
+
+fn check_doc(doc: DocumentFromRequest) -> Vec<Document>{
     println!(" Документ {:?}", doc);
 
-    let mut vec_result_doc: Vec<ResponeDocUser> = Vec::new();
-    let mut clear_doc: bool = true;
     let mut hm = HashMap::new();
 
     if let Some(title) = doc.title {
         hm.insert("title", title);
-        clear_doc = false;
     }
     if let Some(path) = doc.path {
         hm.insert("path", path);
-        clear_doc = false;
     }
+    let mut vec_users_uuid = Vec::with_capacity(2);
     if let Some(author) = doc.author {
-        let respone_users_vec = check_user(Some(author));
         /*
             Сначала получаем вектор из пользователей, документы который будем искать.
             Далее итерируя каждого пользователя ищем документы, которые имеют author_uuid равный user.uuid
             Найденные документы добавляем к релизному вектору
         */
-        for respone_user in respone_users_vec {
-            let db = DataBase::new(PATH_BD);
-            if let ResponeDocUser::ResponeUser(user) = respone_user {
-                let tmp: HashMap<&str, &str> = HashMap::from([("author_uuid", user.uuid.as_str())]);
-                if let Some(doc_vec) = db.get_doc(tmp) {
-                    for doc in doc_vec {
-                        let tmp = doc.clone();
-                        vec_result_doc.push(ResponeDocUser::ResponeDoc(tmp));
-                    }
-                }
-            }
+        for user in check_user(author) {
+            vec_users_uuid.push(user.uuid);
+            //let db = DataBase::new(PATH_BD);
+
+            //let tmp: HashMap<&str, &str> = HashMap::from([("author_uuid", user.uuid.as_str())]);
+            //if let Some(doc_vec) = db.get_doc(tmp) {
+            //    for doc in doc_vec {
+            //        let tmp = doc.clone();
+            //        vec_result_doc.push(ResponeDocUser::ResponeDoc(tmp));
+            //    }
+            //}
+
         }
     }
     if let Some(subject) = doc.subject {
         hm.insert("subject", subject);
-        clear_doc = false;
            }
     if let Some(type_work) = doc.type_work {
         hm.insert("type_work", type_work);
-        clear_doc = false;
     }
     if let Some(number_work) = doc.number_work {
         hm.insert("number_work", number_work);
-        clear_doc = false;
     }
 
-    if !clear_doc {
-        let db = DataBase::new(PATH_BD);
-        if let Some(doc_vec) = db.get_doc(hm) {
-            for doc in doc_vec {
-                let tmp = doc.clone();
-                vec_result_doc.push(ResponeDocUser::ResponeDoc(tmp));
+    let db = DataBase::new(PATH_BD);
+    let mut res_doc_vec = Vec::new();
+
+    if vec_users_uuid.len() != 0 {
+        for user_uuid in &vec_users_uuid {
+            hm.insert("author_uuid", user_uuid);
+            if let Some(doc_vec) = db.get_doc(&hm) {
+                res_doc_vec.extend_from_slice(&doc_vec)
             }
         }
+        return res_doc_vec
     }
-    vec_result_doc
 
+    if hm.len() != 0 {
+        if let Some(doc_vec) = db.get_doc(&hm) {
+            res_doc_vec.extend_from_slice(&doc_vec)
+        }
+        return res_doc_vec
+    }
+    return Vec::new();
 }
+
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(crate = "rocket::serde")]
+struct Task<'r> {
+    description: &'r str,
+    complete: bool
+}
+
+
+#[post("/todo", data = "<task>")]
+fn new_doc(task: Json<Document>) -> Json<String>{
+    println!("{:?}", task);
+    //получить документ
+    return Json(String::from("hello"));
+}
+
 
 #[macro_use] extern crate rocket;
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index, icon])
-    .mount("/api", routes![all_api, get_files, get_val, get_all_users])
+    rocket::build()
+        .mount("/", routes![index, icon])
+        .mount("/api", routes![all_api, get_files])//, get_val, new_doc])
+        .mount("/api_beta", routes![get_val_new])
 }
